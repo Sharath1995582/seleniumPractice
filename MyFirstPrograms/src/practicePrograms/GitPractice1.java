d4ec6d59ca3d5840b211ae7206d24f56207b89b3
Sharath1995582 <mummidisharath@gmail.com>
2023-09-23 01:34:11 +0400

    added to GitPractice1

--- MyFirstPrograms/src/practicePrograms/GitPractice1.java
+++ MyFirstPrograms/src/practicePrograms/GitPractice1.java
@@ -5,7 +5,7 @@ public class GitPractice1 {
 	public static void main(String[] args) {
 		System.out.println("this is git class");
 		System.out.println("modifying");
-
+		System.out.println("checking");
 	}
 
 }
