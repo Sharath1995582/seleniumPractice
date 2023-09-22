8640d05ee1502a8dbc48fb906ea09aa9ad21703b
Sharath1995582 <mummidisharath@gmail.com>
2023-09-23 01:08:55 +0400

    MyFirstPrograms

---
+++ MyFirstPrograms/src/practicePrograms/GitPractice.java
@@ -0,0 +1,14 @@
+package practicePrograms;
+
+public class GitPractice {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is for Git class");
+		System.out.println("Modifying");
+		System.out.println("More Changes");
+		System.out.println("Added new line");
+
+	}
+
+}
---
+++ MyFirstPrograms/src/practicePrograms/GitPractice1.java
@@ -0,0 +1,10 @@
+package practicePrograms;
+
+public class GitPractice1 {
+
+	public static void main(String[] args) {
+		System.out.println("this is git class");
+
+	}
+
+}
